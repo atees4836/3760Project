a54a08c82266cf7bebc3c88fb1b9b1bb95707f01
Alanna Tees <atees@uoguelph.ca>
2021-11-05 14:31:59 -0400

    Added end game button. Not functional

--- Assets/IngameButtonBehaviour.cs
+++ Assets/IngameButtonBehaviour.cs
@@ -1,10 +1,20 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+using UnityEditor;
+
 public class IngameButtonBehaviour : MonoBehaviour
 {
     public void LoadMenu()
     {
         SceneManager.LoadScene("MainMenu");
+    }
+
+    public void EndGame()
+    {
+        if(EditorUtility.DisplayDialog("Are you sure you want to end the game?", "I'm sure","Cancel"))
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
     }
 }
