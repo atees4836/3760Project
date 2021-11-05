d8e84067a7f3cf38b4e853cfd69bb0c8c6695da9
Alanna Tees <atees@uoguelph.ca>
2021-11-05 17:22:24 -0400

    Code review
     - Function comments

--- Assets/IngameButtonBehaviour.cs
+++ Assets/IngameButtonBehaviour.cs
@@ -3,6 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class IngameButtonBehaviour : MonoBehaviour
 {
+    // Returns to main menu
     public void LoadMenu()
     {
         SceneManager.LoadScene("MainMenu");
